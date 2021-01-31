d1101058fafc80704933c360340a16ca748b730b
Glitch (atabotlist) <none>
2021-01-31 20:57:21 +0000

    🛣💫 Checkpoint ./komutlar/yardım.js:24332570/380

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,14 +5,10 @@ const db = require ('quick.db')
 exports.run = async (client, message, args) => {
 const merziki = new Discord.MessageEmbed()
 .setColor('ORANGE')
-.setTitle('» Komutlar')
+.setTitle('Bot Nasıl Eklenir?')
 .setTimestamp(30000)
-.addField(`• ${PREFIX}botekle **<botID> <prefix>**`,`Bot başvurursunuz.`)
-.addField(`• ${PREFIX}davet`,`Botu ekleme linkini alırsınız.`)
-.addField(`• ${PREFIX}istatistik`,`Bot istatisklerini atar.`)
-.addField(`• ${PREFIX}yetkili`,`Yetkili yardm menüsünü atar.`)
-.setFooter('')
-.setTimestamp()
+.addField("• Botunuzu Eklemek İçin; \n\n `-botekle <botID> <prefix>`")
+.setFooter('e')
 message.channel.send(merziki)
 };
 
